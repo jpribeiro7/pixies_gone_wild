4588950b2502ad133a7c4a5784443a2de0e94422
Jonas L <joaohccl@hotmail.com>
2018-12-21 01:12:26 +0000

    need to adjust this

--- public/pixi/sprite2.js
+++ public/pixi/sprite2.js
@@ -22,9 +22,10 @@ window.onload = function() {
         .load(setup);
 
     const aspRatio = app.renderer.width / app.renderer.height;
+    const numberSprites = 144;
 
     function setup() {
-        for(let i  = 0; i < 144; i++){
+        for(let i  = 0; i < numberSprites; i++){
             const sprite = new Sprite(
                 resources[path1].texture
             );
@@ -36,23 +37,28 @@ window.onload = function() {
             sprite.height = sprite.height / aspRatio;
             sprite.x = app.renderer.width / 6;
             sprite.y = i*4;
+            sprite.finalY = (numberSprites-1)*4 - sprite.y;
+            sprite.vy = (sprite.finalY - sprite.y)/200;
 
             app.stage.addChild(sprite);
 
-            app.ticker.add((delta) => {
-                sprite.timer += delta;
-                const timeRatio = sprite.timer/(sprite.id*100);
-                if(sprite.id === 1){
-                    console.log(sprite.timeStart);
-                }
-                if(timeRatio > 1 && (!sprite.canMove || sprite.timer < sprite.timeStart + 200)){
-                    if(!sprite.canMove){
-                        sprite.canMove = true;
-                        sprite.timeStart = sprite.timer;
-                    }
-                    sprite.x += 1 + delta;
-                }
-            });
+            app.ticker.add((delta) => animation(sprite, delta));
+        }
+    }
+
+    function animation(sprite, delta) {
+        sprite.timer += delta;
+
+        //timeRatio is to check when a determined sprite can move
+        const timeRatio = sprite.timer/(sprite.id*100);
+
+        if(timeRatio > 1 && (!sprite.canMove || sprite.timer <= sprite.timeStart + 200)){
+            if(!sprite.canMove){
+                sprite.canMove = true;
+                sprite.timeStart = sprite.timer;
+            }
+            sprite.x += 3 + delta;
+            sprite.y += sprite.vy;
         }
     }
 };
