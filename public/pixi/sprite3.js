9330f6ed2b9511fb30f31af0b96e038984d6ae22
Jonas L <joaohccl@hotmail.com>
2018-12-22 01:56:35 +0000

    :)

--- public/pixi/sprite3.js
+++ public/pixi/sprite3.js
@@ -2,6 +2,9 @@ window.onload = function() {
     let Application = PIXI.Application,
         loader = PIXI.loader,
         resources = PIXI.loader.resources,
+        Container = PIXI.Container,
+        Text = PIXI.Text,
+        TextStyle = PIXI.TextStyle,
         Sprite = PIXI.Sprite;
 
     let app = new Application({
@@ -29,6 +32,7 @@ window.onload = function() {
         let container = new Container();
         let objects = [];
         let counter = 0;
+        let extraWidth = 0;
 
         function getText() {
             let style = new TextStyle({
@@ -46,34 +50,38 @@ window.onload = function() {
             return new Text("Hello", style);
         }
 
+        function getImage() {
+            const sprite = new Sprite(
+                resources[path1].texture
+            );
+            sprite.width = sprite.width / aspRatio;
+            sprite.height = sprite.height / aspRatio;
+            return sprite;
+        }
+
         function gameLoop() {
             //(which happens 60 times per second)
             requestAnimationFrame(gameLoop);
-
             counter+=1;
-
             if(counter > twoMinuteFrames){
+
                 counter = 0;
                 objects = [];
+                app.stage.removeChildren();
+
                 container = new Container();
 
                 for(let i = 0; i < numberElements; i++){
                     let random = Math.random();
-
-                    if(random < 0.5){
-                        let message = getText();
-                        container.addChild(message);
-                        objects.push(message);
+                    let object2add = (random < 0.5) ? getText() : getImage();
+                    if(objects.length !== 0){
+                        object2add.x = objects[i-1].x + objects[i-1].width + app.renderer.width/300;
                     }
-                    else{
-                        const sprite = new Sprite(
-                            resources[path1].texture
-                        );
-                        container.addChild(sprite);
-                        objects.push(sprite);
-                    }
+                    container.addChild(object2add);
+                    objects.push(object2add);
                 }
 
+                container.position.set(app.renderer.width/10, app.renderer.height/100);
                 app.stage.addChild(container);
             }
         }
